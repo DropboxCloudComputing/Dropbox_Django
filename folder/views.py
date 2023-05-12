77852725ebb8057d189f94758e2eb27b2583be9a
3un0ia <kj122kj@naver.com>
2023-05-13 00:01:30 +0900

    Modified views

--- folder/views.py
+++ folder/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
+from .models import Folder
+from .models import Permission
 
-# Create your views here.
+def folder_create(request) :
+    if request.method == 'POST' :
+            data = JSONParser().parse(request)
+            
+
+
+def folder_deleter(request) :
+
+
+def folder_update(request) :
+
+
+def folder_move(request) :
