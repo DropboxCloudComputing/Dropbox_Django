0432a1e2f65ee3a15d41d774dc85035dc5b391d3
hil679 <hil679922@gmail.com>
2023-05-18 15:15:32 +0900

    Add: cheking token and exception for shared folder

--- folder/views.py
+++ folder/views.py
@@ -1,7 +1,6 @@
 from .models import Folder
 from .models import Permission
-from .selializer import CreateFolderSerializer
-from .selializer import FolderSerializer 
+from .selializer import CreateFolderSerializer, FolderSerializer 
 from rest_framework_simplejwt.views import TokenVerifyView
 
 from rest_framework.parsers import JSONParser
@@ -13,11 +12,12 @@ import itertools
 from user_app.models import Users
 # from trashbin.models import Trashbin
 # from trashbin.serializers import TrashbinSerializer
+from django.core.serializers import serialize
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 import jwt
 from dropbox.settings import SECRET_KEY
-
+import json
 
 
 
@@ -227,21 +227,21 @@ def folderVerifyName(request) :
                 #     return response
 
             
+def getSharedFolder(folderId):
+    return Folder.objects.get(id = folderId)
 
 def getSharedFolders(request):
     if request.method == 'GET':
-        auth = JWTAuthentication()
-        token = request.headers.get('Authorization')
         try:
-            #user, t= auth.authenticate(request)
-            # print(token)
-            payload = decode_jwt_token(request.headers.get('Authorization')) #jwt.decode(token, 'SECRET_KEY', algorithms=['HS256'])
-            print(payload)
+            token = request.headers.get('Authorization')
+            payload = decode_jwt_token(token) 
             user = Users.objects.get(id = payload["user_id"])
-            # print(payload)
         except AuthenticationFailed:
             # 유효하지 않은 토큰 또는 인증 오류 발생시 처리할 내용
-            return HttpResponse(status=401)
+            return JsonResponse({
+                    "ResponseCode":401,
+                    "description" : "권한이 없습니다."
+                    }, status=401)
 
         # user = Users.objects.get(email = "xxx@naver.com")#token으로 변경해야 함
         pemissionQueryset = Permission.objects.filter(users = user)
@@ -249,22 +249,81 @@ def getSharedFolders(request):
         folder_data = list(itertools.chain(*[d["folder"] for d in serializer.data]))
         
         return JsonResponse(folder_data, safe = False)
-
-
 def shareFolder(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
         recieverArray = data['reciever']
-        folder = Folder.objects.get(id=data['folderId'])
+        if Folder.objects.filter(id=data['folderId']).exists():
+            folder = Folder.objects.get(id=data['folderId'])
+            try:
+                token = request.headers.get('Authorization')
+                payload = decode_jwt_token(token) 
+                user = Users.objects.get(id=payload["user_id"])
+                if user.id != folder.users.id:
+                    return JsonResponse({
+                        "ResponseCode": 401,
+                        "description": "권한이 없습니다."
+                    }, status=401)
+                if not Permission.objects.filter(users=user, folder=folder).exists():
+                    permission_data = {
+                        'users': user.id,
+                        'folder': folder.id
+                    }
+                    serializer = SharedFolderSerializer(data=permission_data)
+                    if serializer.is_valid():
+                        serializer.save()
+                    else:
+                        return JsonResponse({
+                            "ResponseCode": 400,
+                            "description": "공유하지 못했습니다.",
+                            "errors": serializer.errors
+                        }, status=400)
+            except AuthenticationFailed:
+                return JsonResponse({
+                    "ResponseCode": 401,
+                    "description": "권한이 없습니다."
+                }, status=401)
+        else:
+            return JsonResponse({
+                "ResponseCode": 404,
+                "description": "파일을 찾을 수 없습니다."
+            }, status=404)
+        
+        for recieverEmail in recieverArray:
+            try:
+                reciever = Users.objects.get(email=recieverEmail)
+                # permission = Permission(users=reciever, folder=folder)
+                # print(permission)
+                permission_data = {
+                    'users': reciever.id,
+                    'folder': folder.id
+                }
+                serializer = SharedFolderSerializer(data=permission_data)
+                if serializer.is_valid():
+                    if not Permission.objects.filter(users=reciever, folder=folder).exists():
+                        serializer.save()
+                else:
+                    return JsonResponse({
+                        "ResponseCode": 400,
+                        "description": "공유하지 못했습니다.",
+                        "errors": serializer.errors
+                    }, status=400)
+            except Users.DoesNotExist:
+                return JsonResponse({
+                    "ResponseCode": 404,
+                    "description": "수신자를 찾을 수 없습니다."
+                }, status=404)
+        return JsonResponse({"ResponseCode": 201,
+                "folder": {
+        "id": folder.id,
+        "name": folder.folder_name,
+        "userId": folder.users.id,
+        # 다른 필드들도 필요에 따라 추가할 수 있습니다.
+        },
+        "receiverEmail": recieverArray
+        })
 
-        for reciever in recieverArray:
-            user = Users.objects.get(email = reciever)
-            permission = Permission.objects.create(users=user, folder=folder)
-            serializer = SharedFolderSerializer(data=permission.__dict__)
-            if serializer.is_valid():
-                serializer.save()
-
-        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+ 
     
 # def contentInFolder(request):
 #     if request.method == 'GET':
@@ -274,12 +333,7 @@ def shareFolder(request):
 # 토큰에서 payload 추출
 def decode_jwt_token(token):
     try:
-        print(token)
         payload = jwt.decode(token, SECRET_KEY, 'HS256')
-        
-        print(type(payload))
-        print(payload['user_id'])
-
         return payload
     except jwt.ExpiredSignatureError:
         raise AuthenticationFailed('Token expired')
