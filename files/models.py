40d4c4eea33d7b83b1a23d0bf43e4d7714d6b1d2
hil679 <hil679922@gmail.com>
2023-05-20 13:04:33 +0900

    Add: how to sort

--- files/models.py
+++ files/models.py
@@ -12,7 +12,7 @@ class Files(models.Model):
     folder_id = models.IntegerField(null=True)
     created_at = models.DateTimeField(default=timezone.now)
     last_modified = models.DateTimeField(default=timezone.now)
-    user_id = models.ForeignKey(User, to_field='user_id', on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE, db_column="users_id")
     memo = models.TextField(null=True)
     version = models.IntegerField(default=1)
     removed = models.BooleanField(default=False)
--- files/views.py
+++ files/views.py
@@ -16,6 +16,14 @@ class FileSearch(APIView):
             filesQueryset = Files.objects.filter(user_id=user.id)
         else:
             filesQueryset = Files.objects.filter(user_id=user, file_name__icontains=kwd)
+        
+        if sort == 1:
+            filesQueryset = filesQueryset.order_by("-last_modified")
+        elif sort == 2:
+            filesQueryset = filesQueryset.order_by("file_name")
+        elif sort == 3:
+            filesQueryset = filesQueryset.order_by("size")
+        
 
         serializer = FilesSerializer(filesQueryset, many=True)
         return JsonResponse(serializer.data, safe=False)
