da63d53966555a57e5c73ac2a3f083351ad38c5e
whe0529 <113650390+whe0529@users.noreply.github.com>
2023-05-29 02:09:39 +0900

    API added & modified

--- trashbin/urls.py
+++ trashbin/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import FileRemovetoTrashView, FileRecoverfromTrashView, FilePermanentDeleteView
-
+from .views import *
 
 urlpatterns = [
-    path('remove/<int:file_id>/', FileRemovetoTrashView.as_view(), name='file-remove'),
-    path('recover/<int:file_id>/', FileRecoverfromTrashView.as_view(), name='file-recover'),
-    path('delete/<int:file_id>/', FilePermanentDeleteView.as_view(), name='file-permanent-delete'),
+    path('print', TrashBinListAPI.as_view(), name = 'trashbin-list-print'), 
+    path('clear', TrashBinClearAPI.as_view(), name = 'trashbin-clear'),
+    path('remove/<int:file_id>/', FileRemoveAPI.as_view(), name='file-remove'),
+    path('recover/<int:file_id>/', FileRecoverAPI.as_view(), name='file-recover'),
+    path('delete/<int:file_id>/', FilePermanentlyDeleteAPI.as_view(), name='file-permanently-delete'),
 ]
