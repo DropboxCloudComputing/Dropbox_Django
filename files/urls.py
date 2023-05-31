d194be3309149466321aad2d90ee1d3b3441134a
whe0529 <113650390+whe0529@users.noreply.github.com>
2023-05-31 14:12:15 +0900

    files/urls modified

--- files/urls.py
+++ files/urls.py
@@ -9,9 +9,7 @@ urlpatterns = [
 
     # path('files/', FileListView.as_view(), name='file-show'),
     # path('file/<int:file_id>', FileDetailView.as_view(), name='file-show-by-id'),
-    path('remove/<int:file_id>/', FileRemovetoTrashView.as_view(), name='file-remove'),
-    path('recover/<int:file_id>/', FileRecoverfromTrashView.as_view(), name='file-recover'),
-    path('delete/<int:file_id>/', FilePermanentDeleteView.as_view(), name='file-permanent-delete'),
+    
     path('files/', FileList.as_view(), name = 'File_list'),
     path('files/<int:id>' , FileDetail.as_view(), name = 'File_detail'),
     path('memos/<int:id>' , MemoDetail.as_view(), name = 'Memo_Detail'),
